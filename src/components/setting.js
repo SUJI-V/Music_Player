5cacf4a4ba93926801a5a8ebf4ea85f4427bbfbc
SUJI-V <sujivasanthakumar@gmail.com>
2021-07-13 15:29:35 +0530

    settings page final update

--- src/components/setting.js
+++ src/components/setting.js
@@ -5,12 +5,9 @@ import {
   ScrollView,
   Switch,
   TouchableOpacity,
-  Pressable,
   Image,
   Modal,
   StyleSheet,
-  TextInput,
-  ToastAndroid,
 } from 'react-native';
 import {CheckBox} from 'react-native-elements';
 import Icon from 'react-native-vector-icons/MaterialIcons';
@@ -18,8 +15,8 @@ import AntDesign from 'react-native-vector-icons/AntDesign';
 import {Divider} from 'react-native-paper';
 const Setting = ({navigation}) => {
   const [checkboxValue, setcheckboxValue] = useState(false);
+
   const [modalOpen, setModalOpen] = useState(false);
-  const [isEnabled, setIsEnabled] = useState(false);
 
   const toggleSwitch = () => setIsDisabled(previousState => !previousState);
   const [isDisabled, setIsDisabled] = useState(false);
@@ -44,6 +41,21 @@ const Setting = ({navigation}) => {
 
   const toggleSwitch7 = () => setIsDisabled7(previousState => !previousState);
   const [isDisabled7, setIsDisabled7] = useState(false);
+  
+  const toggleSwitch8 = () => setIsDisabled8(previousState => !previousState);
+  const [isDisabled8, setIsDisabled8] = useState(false);
+  
+  const toggleSwitch9 = () => setIsDisabled9(previousState => !previousState);
+  const [isDisabled9, setIsDisabled9] = useState(false);
+
+  const toggleSwitch10 = () => setIsDisabled10(previousState => !previousState);
+  const [isDisabled10, setIsDisabled10] = useState(false);
+
+  const toggleSwitch11= () => setIsDisabled11(previousState => !previousState);
+  const [isDisabled11, setIsDisabled11] = useState(false);
+
+  const toggleSwitch12= () => setIsDisabled12(previousState => !previousState);
+  const [isDisabled12, setIsDisabled12] = useState(false);
 
  
   return (
@@ -155,13 +167,12 @@ const Setting = ({navigation}) => {
               }}>
               
               <AntDesign
-                name="search1" size={25} color="#fff"   onPress={() => navigation.navigate('Library')}/>
+                name="gift" size={25} color="#fff"    onPress={() => navigation.navigate('Ads')}/>
             </View>
           </View>
         </View>
 
-  
-      <ScrollView style={{height: '92%',backgroundColor: '#376f8aaa',}}>
+      <ScrollView style={{height: '100%',backgroundColor: '#376f8aaa',}}>
 
         <View style={styles.headings} >
           <Text style={{ color: '#FFDD00', fontWeight:"bold", }}> Normal Settings
@@ -169,7 +180,7 @@ const Setting = ({navigation}) => {
         </View>
           <View style={styles.container}>
           <View style={styles.TextView}>
-          <Text style={styles.TextStyles}>Desktop Lyrics</Text>
+          <Text style={styles.TextStyles1}>Desktop Lyrics</Text>
           <Text style={styles.TextStyles}>Desktop Lyrics closed</Text>
             </View>
             <View style={styles.SwitchView}>
@@ -183,10 +194,9 @@ const Setting = ({navigation}) => {
             </View>
           </View>
           
-          
           <View style={styles.container}>
           <View style={styles.TextView}>
-          <Text style={styles.TextStyles}>Lock screen playing</Text>
+          <Text style={styles.TextStyles1}>Lock screen playing</Text>
           <Text style={styles.TextStyles}>show now playing when lock screen</Text>
             </View>
             <View style={styles.SwitchView}>
@@ -202,34 +212,32 @@ const Setting = ({navigation}) => {
           
           <View style={styles.container}>
           <View style={styles.TextView}>
-          <Text style={styles.TextStyles}>Show Shuffle button</Text>
+          <Text style={styles.TextStyles1}>Show Shuffle button</Text>
           <Text style={styles.TextStyles}>Show custom shuffle button</Text>
             </View>
             <View style={styles.SwitchView}>
             <CheckBox
-            
               checked={checkboxValue}
               onPress={() => setcheckboxValue(!checkboxValue)}
             />
             </View>
           </View>
           
-          
           <TouchableOpacity style={styles.container} onPress={() => setModalOpen(true)}>
           <View style={styles.TextView}>
-          <Text style={styles.TextStyles}>Lock screen Time Format</Text>
+          <Text style={styles.TextStyles1}>Lock screen Time Format</Text>
           <Text style={styles.TextStyles}>12 Hours/24 Hours</Text>
             </View>
             <View style={styles.SwitchView}>
             <Icon
                 name="arrow-right"
-                size={30}
+                color={"#fff"}
+                size={35}
                 
               />
             </View>
           </TouchableOpacity>
-          
-          
+
           <View style={styles.headings} >
           <Text style={{ color: '#FFDD00', fontWeight:"bold", }}> Audio Settings
           </Text>
@@ -237,7 +245,7 @@ const Setting = ({navigation}) => {
 
         <View style={styles.container}>
           <View style={styles.TextView}>
-          <Text style={styles.TextStyles}>Volume Fade in and fade out</Text>
+          <Text style={styles.TextStyles1}>Volume Fade in and fade out</Text>
           
             </View>
             <View style={styles.SwitchView}>
@@ -253,7 +261,7 @@ const Setting = ({navigation}) => {
           
           <View style={styles.container}>
           <View style={styles.TextView}>
-          <Text style={styles.TextStyles}>Shake To Play Next Song</Text>
+          <Text style={styles.TextStyles1}>Shake To Play Next Song</Text>
          
             </View>
             <View style={styles.SwitchView}>
@@ -269,7 +277,7 @@ const Setting = ({navigation}) => {
           
           <View style={styles.container}>
           <View style={styles.TextView}>
-          <Text style={styles.TextStyles}>Open nowplaying on play</Text>
+          <Text style={styles.TextStyles1}>Open nowplaying on play</Text>
           <Text style={styles.TextStyles}>click tracks to open nowplaying songs</Text>
             </View>
             <View style={styles.SwitchView}>
@@ -285,7 +293,7 @@ const Setting = ({navigation}) => {
           
           <View style={styles.container}>
           <View style={styles.TextView}>
-          <Text style={styles.TextStyles}>Replay the song</Text>
+          <Text style={styles.TextStyles1}>Replay the song</Text>
           <Text style={styles.TextStyles}>Replay when click the current playing song</Text>
             </View>
             <View style={styles.SwitchView}>
@@ -301,20 +309,155 @@ const Setting = ({navigation}) => {
          
 
 
-
-        <View style={styles.headings} >
-          <Text style={{ color: '#FFDD00', fontWeight:"bold", }}> Playlist Settings
-          </Text>
-        </View>
-
         <View style={styles.headings} >
           <Text style={{ color: '#FFDD00', fontWeight:"bold", }}> Headset Settings
           </Text>
         </View>
+
+        <View style={styles.container}>
+          <View style={styles.TextView}>
+          <Text style={styles.TextStyles1}>Play when Inserted</Text>
+          <Text style={styles.TextStyles}>Auto play when headset connected</Text>
+            </View>
+            <View style={styles.SwitchView}>
+            <Switch
+              trackColor={{false: '#767577', true: '#FFD700'}}
+              thumbColor={isDisabled7 ? '#FFD700' : '#f4f3f4'}
+              ios_backgroundColor="#3e3e3e"
+              onValueChange={toggleSwitch7}
+              value={isDisabled7}
+            />
+            </View>
+          </View>
+        
+          <View style={styles.container}>
+          <View style={styles.TextView}>
+          <Text style={styles.TextStyles1}>Play when unplugged</Text>
+          <Text style={styles.TextStyles}>Auto pause when heaadset unplugged</Text>
+            </View>
+            <View style={styles.SwitchView}>
+            <Switch
+              trackColor={{false: '#767577', true: '#FFD700'}}
+              thumbColor={isDisabled8 ? '#FFD700' : '#f4f3f4'}
+              ios_backgroundColor="#3e3e3e"
+              onValueChange={toggleSwitch8}
+              value={isDisabled8}
+            />
+            </View>
+          </View>
+        
+          <View style={styles.container}>
+          <View style={styles.TextView}>
+          <Text style={styles.TextStyles1}>Blutooth Autostart</Text>
+          <Text style={styles.TextStyles}>Auto Play when blutooth headset connected</Text>
+            </View>
+            <View style={styles.SwitchView}>
+            <Switch
+              trackColor={{false: '#767577', true: '#FFD700'}}
+              thumbColor={isDisabled9 ? '#FFD700' : '#f4f3f4'}
+              ios_backgroundColor="#3e3e3e"
+              onValueChange={toggleSwitch9}
+              value={isDisabled9}
+            />
+            </View>
+          </View>
+        
+          <View style={styles.container}>
+          <View style={styles.TextView}>
+          <Text style={styles.TextStyles1}>Blutooth Autostop</Text>
+          <Text style={styles.TextStyles}>Auto pause when blutooth headset disconnects</Text>
+            </View>
+            <View style={styles.SwitchView}>
+            <Switch
+              trackColor={{false: '#767577', true: '#FFD700'}}
+              thumbColor={isDisabled10 ? '#FFD700' : '#f4f3f4'}
+              ios_backgroundColor="#3e3e3e"
+              onValueChange={toggleSwitch10}
+              value={isDisabled10}
+            />
+            </View>
+          </View>
+        
+          <View style={styles.container}>
+          <View style={styles.TextView}>
+          <Text style={styles.TextStyles}>Headset Control Allowed</Text>
+          <Text style={styles.TextStyles}>Allow player control over headset</Text>
+            </View>
+            <View style={styles.SwitchView}>
+            <Switch
+              trackColor={{false: '#767577', true: '#FFD700'}}
+              thumbColor={isDisabled11 ? '#FFD700' : '#f4f3f4'}
+              ios_backgroundColor="#3e3e3e"
+              onValueChange={toggleSwitch11}
+              value={isDisabled11}
+            />
+            </View>
+          </View>
+        
         <View style={styles.headings} >
           <Text style={{ color: '#FFDD00', fontWeight:"bold", }}> Others
           </Text>
         </View>
+
+        <View style={styles.container}>
+          <View style={styles.TextView}>
+          <Text style={styles.TextStyles1}>Use English Language</Text>
+          
+            </View>
+            <View style={styles.SwitchView}>
+            <Switch
+              trackColor={{false: '#767577', true: '#FFD700'}}
+              thumbColor={isDisabled12 ? '#FFD700' : '#f4f3f4'}
+              ios_backgroundColor="#3e3e3e"
+              onValueChange={toggleSwitch12}
+              value={isDisabled12}
+            />
+            </View>
+          </View>
+   
+          <View style={styles.container}>
+          <View style={styles.TextView}>
+          <Text style={styles.TextStyles1}>Hot App</Text>
+        
+            </View>
+            <View style={styles.SwitchView}>
+           
+            </View>
+          </View>
+   
+
+          <View style={styles.container}>
+          <View style={styles.TextView}>
+          <Text style={styles.TextStyles1}>Stop Ads</Text>
+          
+            </View>
+            <View style={styles.SwitchView}>
+          
+            </View>
+          </View>
+   
+
+          <View style={styles.container}>
+          <View style={styles.TextView}>
+          <Text style={styles.TextStyles1}>Rate For Us</Text>
+          
+            </View>
+            <View style={styles.SwitchView}>
+            
+            </View>
+          </View>
+   
+
+          <View style={styles.container}>
+          <View style={styles.TextView}>
+          <Text style={styles.TextStyles1}>Share This App</Text>
+        
+            </View>
+            <View style={styles.SwitchView}>
+           
+            </View>
+          </View>
+   
 
       </ScrollView>
     </>
@@ -323,50 +466,40 @@ const Setting = ({navigation}) => {
 const styles = StyleSheet.create({
     container: {
       width: '100%',
-      height:80,
-      paddingLeft: '5%',
+      height:70,
+      paddingLeft: '3%',
       flexDirection: 'row',
      alignItems: 'center',
      justifyContent: 'space-between',
-      borderBottomWidth:1,
-      borderColor:"#FFFFFF"
+      backgroundColor: '#ffffff40',
       
     },
     headings:{
       justifyContent:"center",
       alignItems:"flex-start",
-      height:"6%",
-      paddingLeft:"5%",
-    },
-    sections:{
-      backgroundColor: '#ffffff10',
-      marginBottom:"5%",
+      height:35,
+      paddingLeft:"3%",
     },
     TextView:{
 height:"100%",
-borderWidth:1,
-borderColor:"red",
 width:"80%",
 alignItems:"flex-start",
 justifyContent:"center",
     },
     SwitchView:{
       height:"100%",
-      borderWidth:1,
-      borderColor:"red",
       width:"20%",
       alignItems:"center",
 justifyContent:"center",
     },
     TextStyles:{
-      color:"#FFFFFF"},
-
-  centeredView: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 42,
-  },
+      color:"#FFFFFF"
+    },
+    TextStyles1:{
+        color:"#FFFFFF",
+        fontSize:16,
+        fontWeight:"bold",
+      },
   modalView: {
     margin: 10,
     bottom: 50,
